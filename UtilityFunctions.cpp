1b6914ae0f3ec39e992e86af3711566be361fb14
Griffen Cook <griffenck1@gmail.com>
2020-02-11 18:44:19 +0000

    Implemented MatchVectors function

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -1,1 +1,17 @@
 #include "UtilityFunctions.h"
+
+// takes two vectors of integers, a and b. The function then removes elements from a if they are also in b.
+// If the integer is in b, but not in a, nothing happens.
+std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b){
+	std::vector<int>::iterator it;
+	it = a.begin();
+	for(unsigned int i = 0; i < a.size(); i++){
+		it += 1;
+		for(unsigned int i2 = 0; i2 < b.size(); i2++){
+			if(a[i] == a[i2]){
+				a.erase(it);
+			}
+		}
+	}
+	return a;
+}
--- UtilityFunctions.h
+++ UtilityFunctions.h
@@ -1,5 +1,9 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <iostream>
+#include <vector>
+
+std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
 
 #endif
