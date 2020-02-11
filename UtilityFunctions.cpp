bc57b3c4bbb4ce5fda9e91433aa89e69f6931c32
Julian Lambert <julian.lambert@colorado.edu>
2020-02-11 11:22:28 -0700

    Implemented Sum function
    Added Sum definition to UtilityFunctions.h and UtilityFunctions.cpp

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -1,1 +1,10 @@
 #include "UtilityFunctions.h"
+
+// Sums all numbers in a vector and returns the resulting value
+double Sum(std::vector<double> nums) {
+  double acc = 0;
+  for (int i = 0; i < nums.size(); i++) {
+    acc += nums[i];
+  }
+  return acc;
+}
--- UtilityFunctions.h
+++ UtilityFunctions.h
@@ -1,5 +1,14 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+/**
+  Sum adds all numbers in a vector.
+
+  @params:
+    num - is a std::vector<double> of numbers to be summed
+  @output:
+    all numbers in nums summed together
+*/
+double Sum(std::vector<double> nums);
 
 #endif
