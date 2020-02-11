dd3ce4c56d12a23f1e7585de98b2db4c9924cf5a
HenrysElephant1 <jacu9594@colorado.edu>
2020-02-11 11:34:35 -0700

    Added MultiplesFilter function

--- UtilityFunctions.h
+++ UtilityFunctions.h
@@ -1,5 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <vector>
+#include <iostream>
+
+// Jason
+// takes a vector of integers and removes all elements evenly divisible by the passed in int
+std::vector<int> MultiplesFilter(std::vector<int>, int divides_by);
 
 #endif
